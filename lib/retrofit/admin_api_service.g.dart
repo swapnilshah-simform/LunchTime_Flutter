0d9bd2a317a09b3017d58c7a2052613025d09d34
Swapnil Shah <swapnil.s@simformsolutions.com>
2022-04-02 10:52:01 +0530

    feature/admin page final

---
+++ lib/retrofit/admin_api_service.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'admin_api_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+class _ApiClient implements ApiClient {
+  _ApiClient(this._dio, {this.baseUrl}) {
+    baseUrl ??= 'http://172.16.3.143:8000/';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<HttpResponse<dynamic>> addMenu(user) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(user.toJson());
+    final _result = await _dio.fetch(_setStreamType<HttpResponse<dynamic>>(
+        Options(method: 'POST', headers: _headers, extra: _extra)
+            .compose(_dio.options, 'apimenu/menu/',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data;
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
---
+++ lib/utils/themes.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lunchtime_frontend/utils/palette.dart';
+
+class CustomTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primarySwatch: Palette.primarySwatch,
+      backgroundColor: Colors.black,
+      textTheme: const TextTheme(
+          headline1: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          headline2: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+          headline3: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          subtitle2: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.grey,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          button: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+          ),
+          caption: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+          subtitle1: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 13,
+          ),
+          bodyText1: TextStyle(
+            decoration: TextDecoration.none,
+            color: Colors.black,
+            fontSize: 13,
+          )),
+      appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+          foregroundColor: Colors.teal,
+          titleTextStyle: TextStyle(
+              fontSize: 22, color: Colors.teal, fontWeight: FontWeight.bold)),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedIconTheme: IconThemeData(color: Colors.grey),
+        selectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        elevation: 0,
+      ),
+      floatingActionButtonTheme:
+          const FloatingActionButtonThemeData(backgroundColor: Colors.blueGrey),
+      buttonTheme: const ButtonThemeData(buttonColor: Colors.blueGrey),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      primarySwatch: Colors.blueGrey,
+      backgroundColor: Colors.black,
+      textTheme: const TextTheme(
+        headline1: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        headline2: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+        headline3: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+        button: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+        headline4: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        caption: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 12,
+          fontWeight: FontWeight.bold,
+        ),
+        subtitle1: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 13,
+        ),
+        bodyText1: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 13,
+        ),
+        subtitle2: TextStyle(
+          decoration: TextDecoration.none,
+          color: Colors.black87,
+          fontSize: 15,
+        ),
+        headline6: TextStyle(
+          fontWeight: FontWeight.bold,
+          decoration: TextDecoration.none,
+          color: Colors.black,
+          fontSize: 15,
+        ),
+      ),
+      appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+          foregroundColor: Colors.blueGrey,
+          titleTextStyle: TextStyle(
+              fontSize: 22,
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold)),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        selectedIconTheme: IconThemeData(color: Colors.blueGrey),
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.shifting,
+        elevation: 0,
+      ),
+      floatingActionButtonTheme:
+          const FloatingActionButtonThemeData(backgroundColor: Colors.blueGrey),
+      buttonTheme: const ButtonThemeData(buttonColor: Colors.blueGrey),
+    );
+  }
+}
